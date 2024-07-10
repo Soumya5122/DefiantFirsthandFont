c8ef1a282877d6e1286e7d318a9dd6027f36b8e9
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2024-07-10 07:42:22 +0000

    commit

---
+++ .ccls-cache/@home@runner@DefiantFirsthandFont/4Strings.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s= "hello";
+  cout<<s.size()<<endl;
+  cout<<s.length()<<endl;
+  cout<<s.at(0)<<endl;
+  cout<<s[0];
+  return 0;
+}

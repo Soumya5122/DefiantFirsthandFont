256c96ea11912fb435fd9993d48956db1408d5d1
Soumya5122 <97490356+Soumya5122@users.noreply.github.com>
2024-07-14 06:27:27 +0000

    ..

---
+++ 1.java
@@ -0,0 +1,8 @@
+import java.utilArrays;
+class Main{
+  public static void main(String[] args){
+    int a[]={1,2,34,5,6,7,8,9};
+    Arrays.sort(a);
+    System.out.println(Arrays.toString(a));
+  }
+}
